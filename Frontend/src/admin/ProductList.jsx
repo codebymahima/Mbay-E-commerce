67cfac9d5adcb11d4646e40b4042f3f946c5a4a4
Mahima Lalwani <lalwni.mahi@gmail.com>
2026-09-20 23:59:04 +0530

    Added Add product feature in admin dashboard

---
+++ Frontend/src/admin/ProductList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router'
+
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+  return (
+    <div>ProductList</div>
+  )
+}
+
+export default ProductList
